a8625873abb9b017677275d5e8360aa6ff86a956
tarekcodeshend <112010550+tarekcodeshend@users.noreply.github.com>
2024-09-10 03:27:01 +0300

    Update config.js

--- Fixedcode/Frontend/src/config.js
+++ Fixedcode/Frontend/src/config.js
@@ -3,7 +3,7 @@
 export const production = false;
 
 export default {
-  api: production ? "https://api.bloxpvp.com" : "https://ggshelpice-vwiv.onrender.com/",
+  api: production ? "https://ggshelpice-vwiv.onrender.com/",
   h_captcha_key: production
     ? "495be111-f6a7-4ca5-9b8f-d0149998a742"
     : "20000000-ffff-ffff-ffff-000000000002",
